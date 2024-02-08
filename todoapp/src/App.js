533768e966eff475dd4fbae27da5f091db49f2ce
MakarenkoOleksandr <majop11111@gmail.com>
2024-02-09 00:19:48 +0100

    done todoapp

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputText.trim() !== "") {
+      setTodos([...todos, { text: inputText, done: false }]);
+      setInputText("");
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  const handleToggleDone = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].done = !updatedTodos[index].done;
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={inputText}
+          onChange={handleInputChange}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleAddTodo();
+            }
+          }}
+        />
+
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <span
+              style={{ textDecoration: todo.done ? "line-through" : "none" }}
+              onClick={() => handleToggleDone(index)}
+            >
+              {todo.text}
+            </span>
+            {hoveredIndex === index && (
+              <span
+                className="delete-icon"
+                onClick={() => handleRemoveTodo(index)}
+              >
+                &#128465;
+              </span>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
